aaeea37eef328ddcb67090bc6d726e47532c3f99
jamillypl <jamillyp@alu.ufc.br>
2022-03-09 17:47:26 -0300

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+   <>
+     <Head>
+       <title>home | ig.news</title>
+     </Head>
+
+     <h2>ig.news</h2>
+   </>
+  )
+}
