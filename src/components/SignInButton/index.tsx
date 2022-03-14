a34ae3f452a416adee3ed151e7beb12951d1e256
jamillypl <jamillyp@alu.ufc.br>
2022-03-14 11:20:53 -0300

    Fazendo autenticação com Github

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,19 +1,25 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
 
+import { signIn, useSession } from 'next-auth/react';
+
 import styles from './styles.module.scss';
 
 export function SignInButton(){
-    const isUserLoggedIn = true;
+    const { data: session } = useSession()
 
-    return isUserLoggedIn ? (
+    return session ? (
         <button type="button" className={styles.signInButton}>
             <FaGithub color='#04d361' />
-            Jamilly Pinheiro
+            {session.user.name}
             <FiX color='#737380' className={styles.closeIcon} />
         </button>
     ): (
-        <button type="button" className={styles.signInButton}>
+        <button 
+        type="button" 
+        className={styles.signInButton}
+        onClick={() => signIn()}
+        >
             <FaGithub color='#eba417' />
             Entre com o Github
         </button>
