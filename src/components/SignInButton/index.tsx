83debf1f7857e4419716b3216190ab435e17c51f
jamilly.pinheiro <jamilly.pinheiro@teclat.com.br>
2022-03-29 18:50:41 -0300

    Adicionando o stripe

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,7 +1,7 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
 
-import { signIn, useSession } from 'next-auth/react';
+import { signIn, signOut , useSession } from 'next-auth/react';
 
 import styles from './styles.module.scss';
 
@@ -9,7 +9,11 @@ export function SignInButton(){
     const { data: session } = useSession()
 
     return session ? (
-        <button type="button" className={styles.signInButton}>
+        <button 
+        type="button" 
+        className={styles.signInButton}
+        onClick={() => signOut()}
+        >
             <FaGithub color='#04d361' />
             {session.user.name}
             <FiX color='#737380' className={styles.closeIcon} />
